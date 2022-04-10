9117e2bfa413c2c4b1a036d4df8409dc8bd94875
nicholasqnguyen8 <nicholasqnguyen8@gmail.com>
2022-04-09 22:46:47 -0400

    got square to appear on click

--- archer.c
+++ archer.c
@@ -8,17 +8,47 @@ const bool False = 0;
 const int screenWidth = 800;
 const int screenHeight = 450;
 
+typedef struct Arrow
+{
+    Vector2 position;
+    float hVelocity;
+    float vVelocity;
+} Arrow;
+
+void shoot()
+{
+    
+}
 
 int main()
 {
+    Vector2 position = (Vector2){0.0, 0.0};
+    bool arrowGenerated = False;
+    Rectangle arrow = {};
+
     InitWindow(screenWidth, screenHeight, "Archer");
     SetTargetFPS(60);
 
     while (!WindowShouldClose())
     {
+        // Get mouse position on left click
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+        {
+            position = GetMousePosition();
+            arrow.x = position.x;
+            arrow.y = position.y;
+            arrow.width = 20;
+            arrow.height = 20;
+            arrowGenerated = True;
+        }
+        // Draw
         BeginDrawing();
             ClearBackground(RAYWHITE);
             DrawText("Hello world!", 190, 200, 20, RED);
+            if (arrowGenerated)
+            {
+                DrawRectangleRec(arrow, BLUE);
+            }
         EndDrawing();
     }
 
